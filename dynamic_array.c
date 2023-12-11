17e6dffd4ef463ba34f79d59d76c78ff522a3b47
Nikos Sotirakis <139043572+VonCroyy@users.noreply.github.com>
2023-12-11 23:40:14 +0200

    Update dynamic_array.c

--- dynamic_array.c
+++ dynamic_array.c
@@ -5,99 +5,54 @@ typedef struct {
     int *element;
     int size;
     int cap;
-} dynamic_arr;
+} arithmetic;
 
 int main() {
-    dynamic_arr arr, arr_2;
-    int arr_num, user_inp, choice, element;
+    arithmetic dynamicArray;
+    int usr_inp, usr_num;
+    int i;
 
-    printf("Give the size of the array: ");
-    scanf("%d", &arr_num);
+    printf("Give the size of the numbers: ");
+    scanf("%d", &usr_inp);
 
-    printf("----------------------------\n");
+    printf("--------------------------------------\n");
 
     //initialise array
-    arr.element = calloc(1, sizeof(*arr.element));
-    arr.size = 0;
-    arr.cap = 1;
+    dynamicArray.size = 0;
+    dynamicArray.element = calloc(1, sizeof(*dynamicArray.element));
+    dynamicArray.cap = 1;
 
-    //Checking for null return value and giving values to the array
-    if (arr.element != NULL) {
-        for (int i=0; i < arr_num; i++) {
-            printf("Give the %d number of the array: ", i + 1);
-            scanf("%d", &user_inp);
-            arr.element = realloc(arr.element, (arr.cap + 1) * sizeof(*arr.element));
-            if (arr.element != 0) {
-                arr.element[arr.size] = user_inp;
-                arr.size++;
-                arr.cap++;
-            }
+    //checking for NULL return value && adjust memory as needed
+    if(dynamicArray.element != NULL) {
+        for(i=0; dynamicArray.size < usr_inp; i++) {
+            printf("Give the %dst number: ", i + 1);
+            scanf("%d", &usr_num);
+            dynamicArray.element = realloc(dynamicArray.element, (dynamicArray.cap + 1) * sizeof(*dynamicArray.element));
+            if(dynamicArray.element != NULL) {
+                dynamicArray.element[dynamicArray.size] = usr_num; 
+                dynamicArray.size ++;
+                dynamicArray.cap ++;
+            } 
             else {
-                printf("The Memory Reallocation Failed!");
+                printf("The memory reallocation failed!");
+                return 1;
             }
         }
-    }
+    } 
     else {
-        printf("The Memory Allocation Failed!");
-    }
-
-    //Checking if the user want to decrease the array
-    printf("----------------------------\n");
-    printf("1 for decrease | 2 for continue: ");
-    scanf("%d", &choice);
-    printf("----------------------------\n");
-
-    if (choice == 1) {
-        //Printing the elements of the list for the user
-        for (int i=0; i < arr.size; i++) {
-            printf("The %d element of the array is: %d\n", i + 1, arr.element[i]);
-        }
-        
-        //Initialising second array
-        arr_2.element = calloc(arr.size, sizeof(*arr_2.element));
-        arr_2.size = 0;
-        arr_2.cap = arr.size;
-
-        printf("----------------------------\n");
-        printf("Give the element you want to remove: ");
-        scanf("%d", &element);
-        printf("----------------------------\n");
-        
-        //Removing an element from the list
-        if (arr_2.element != NULL) {
-            for (int i=0; i < arr_2.cap; i++) {
-                if (i != element - 1) {
-                    arr_2.element[arr_2.size] = arr.element[i];
-                    arr_2.size++;
-                }
-                else {
-                    continue;
-                }
-            }
-            free(arr.element);
-        }
-        else {
-            printf("The Memory Allocatio Failed!");
-        }
-    }
-    else {
-        printf("The Program Has Ended!");
-        free(arr.element);
+        printf("The memory allocation failled!");
         return 1;
     }
 
-    //Printing new array elements
-    for (int i=0; i < arr_2.size; i++) {
-        printf("The %d element of the array is: %d\n", i + 1, arr_2.element[i]);
-        if (i == arr_2.size - 1) {
-            printf("----------------------------\n");
-            printf("The Program Has Ended!");
-        }
+    printf("--------------------------------------\n");
+
+    //printing the elements of the array
+    for(i=0; i < dynamicArray.size; i++) {
+        printf("The %dst element of the Dynamically Allocated Array is: %d\n", i, dynamicArray.element[i]);
     }
 
-    if (choice == 1) {
-        free(arr_2.element);
-    }
+    //freeing the memory after use
+    free(dynamicArray.element);
 
     return 0;
 }
